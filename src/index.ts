3e6af94a7f753309ca0ed3664b2aed4432d6006d
Leonid Netrebskii <leonid.netrebskii@rentready.com>
2021-02-20 16:23:07 +0400

    Add azure function

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as azure from "@pulumi/azure";
+
+// Create an Azure Resource Group
+const resourceGroup = new azure.core.ResourceGroup(`stella-to-client-portal-integration-${pulumi.getStack()}`);
+
+// Create an Azure resource (Storage Account)
+const account = new azure.storage.Account("storage", {
+    // The location for the storage account will be derived automatically from the resource group.
+    resourceGroupName: resourceGroup.name,
+    accountTier: "Standard",
+    accountReplicationType: "LRS",
+});
+
+const serviceBus = new azure.servicebus.Namespace(`client-portal-db-servicebus-${pulumi.getStack()}`, {
+    location: resourceGroup.location,
+    resourceGroupName: resourceGroup.name,
+    sku: "Basic",
+});
+
+const entitiesQueue = new azure.servicebus.Queue("entitiesQueue", {
+    resourceGroupName: resourceGroup.name,
+    namespaceName: serviceBus.name,
+    enablePartitioning: true
+});
+
+const funcAppInsights = new azure.appinsights.Insights(`entitiesPersister-${pulumi.getStack()}`, {
+    location: resourceGroup.location,
+    resourceGroupName: resourceGroup.name,
+    applicationType: "Node.JS",
+});
+export const instrumentationKey = funcAppInsights.instrumentationKey;
+export const appId = funcAppInsights.appId;
+
+const handler = function(context: any, crmContext: any) 
+{
+
+}
+
+const eventFunction = entitiesQueue.getEventFunction('entitiesPersister', handler);
+
+const appservicePlan = new azure.appservice.Plan(`entitiesPersisterApp-${pulumi.getStack()}`, {
+    resourceGroupName: resourceGroup.name,
+    sku: 
+    { 
+        tier: "Dynamic", 
+        size: "Y1" 
+    },
+});
+
+const app = new azure.appservice.MultiCallbackFunctionApp(`entitiesPersisterApp-${pulumi.getStack()}`, {
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    functions: [eventFunction],
+    plan: appservicePlan,
+    account: account,
+    osType: "linux",
+    appSettings: {
+        'APPINSIGHTS_INSTRUMENTATIONKEY': instrumentationKey
+    }
+ });
+
+// Export the connection string for the storage account
+export const connectionString = account.primaryConnectionString;
